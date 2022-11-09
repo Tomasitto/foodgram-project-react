feed7394894a11a7946d149dae99998d86777153
Tomasitto <ogurec98@yandex.ru>
2022-11-09 12:53:14 +0000

    some cnhanges workflow

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,7 +17,7 @@ router_v1.register(r'recipes', RecipesViewSet, basename='recipes')
 
 
 urlpatterns = [
-    #path('', include(router_v1.urls)),
+    # path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include(router_v1.urls)),
