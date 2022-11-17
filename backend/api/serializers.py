d860230c9ad28d67ae83c6d89b762e7e96b7e753
Artem <101455601+Tomasitto@users.noreply.github.com>
2022-11-17 15:32:03 +0300

    add bulk_create

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -191,13 +191,12 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         tags = validated_data.pop('tags')
         recipe = Recipe.objects.create(**validated_data)
         recipe.tags.set(tags)
-        for ingredient in ingredients:
-            id = ingredient.get('id')
+        data = [IngredientRecipe(
+            recipe=recipe,
+            ingredient=get_object_or_404(Ingredient, id=ingredient.get('id')),
             amount = ingredient.get('amount')
-            ingredient_id = get_object_or_404(Ingredient, id=id)
-            IngredientRecipe.objects.create(
-                recipe=recipe, ingredient=ingredient_id, amount=amount
-            )
+        ) for ingredient in ingredients]
+        IngredientRecipe.objects.bulk_create(data)
         recipe.save()
         return recipe
 
