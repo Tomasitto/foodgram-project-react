83684effc5e4bb81c1b3e7dc68c2a9a3aece5c3b
Artem <101455601+Tomasitto@users.noreply.github.com>
2022-11-22 08:37:24 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -22,14 +22,14 @@ class RecipeFilter(filters.FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(
+            return Recipe.objects.filter(
                 favorite_recipe__user=self.request.user
             )
         return Recipe.objects.all()
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(shopping_cart__user=self.request.user)
+            return Recipe.objects.filter(shopping_cart__user=self.request.user)
         return Recipe.objects.all()
 
 
